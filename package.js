87c97e4366264570fff6b6221423eea33da41c13
Adithep SriNarula <badsxx@gmail.com>
2014-08-25 08:34:00 +0700

    cry

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
   summary: "Basic template control",
-  version: "0.0.1"
+  version: "0.0.1",
+  name: "bads:alpha-template"
 });
 
 Package.on_use(function (api, where) {
   api.versionsFrom("METEOR-CORE@0.9.0-atm");
   api.use([
-    'bads:core-lib', 
+    'bads:core-lib',
     'bads:utilities',
     'bads:alpha-stylus',
     'coffeescript',
@@ -16,7 +17,7 @@ Package.on_use(function (api, where) {
 });
 
 Package.on_test(function (api) {
-  api.use("../packages/bads:alpha-template");
+  api.use("bads:alpha-template");
 
   api.add_files('template-control_tests.js', ['client', 'server']);
 });
